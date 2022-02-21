4952ca0f93525fa1272848833cde5e9450ad69d1
nkoubamarius <nkoubamarius@gmail.com>
2022-02-21 16:12:01 +0100

    Define general styles

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Service from './components/services/Service'
+import Testimonial from './components/testimonials/Testimonial'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Nav/>
+      <About/>
+      <Experience/>
+      <Service/>
+      <Testimonial/>
+      <Contact/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
